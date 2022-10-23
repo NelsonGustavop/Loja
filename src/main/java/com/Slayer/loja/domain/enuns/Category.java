1097bcfa66379fb50a4912f4ef3ff72d44e5c8be
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-10-23 19:16:36 -0300

    Enuns class created

--- src/main/java/com/Slayer/loja/domain/enuns/Category.java
+++ src/main/java/com/Slayer/loja/domain/enuns/Category.java
@@ -1,7 +1,16 @@
 package com.Slayer.loja.domain.enuns;
 
 public enum Category {
-	ELETRONICOS,
-	ROUPAS,
-	ALIMENTOS
+	ELETRONICOS(0,"ROLE_ELETRONICOS"),ROUPAS(1,"ROLE_ROUPAS"),ALIMENTOS(2,"ROLE_ALIMENTOS");
+	
+	private Integer codigo;
+	private String tipo;
+	private Category(Integer codigo, String tipo) {
+		this.codigo = codigo;
+		this.tipo = tipo;
+	}
+	
+
+
+	
 }
